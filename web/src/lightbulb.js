64c9951001b0bec8681a6268598b253b1018e7b6
Lisa Walkosz-Migliacio <evilisa@gatech.edu>
2020-04-12 22:05:50 -0500

    get lightbulb ready to input country examples in json

--- web/src/lightbulb.js
+++ web/src/lightbulb.js
@@ -3,27 +3,37 @@ function showCountry(value) {
   let titleDiv = document.getElementById("top5-title");
   let summaryDiv = document.getElementById("top5-summary");
   let bulbDiv = document.getElementById("bulb"+value);
-  console.log(bulbDiv);
+
   let array = [
     {
-      "country": "Country Name1",
-      "summary": "This is a summary for...",
+      "country": "High Innovation Countries",
+      "summary": "Famous for new technology and advancements, these countries can be seen as something to strive towards.<br/><br/>"+
+        "Countries include:<br/>"+
+        "<br/>"+
+        "<br/>"+
+        "<br/>",
     },
     {
-      "country": "Country Name2",
-      "summary": "This is a summary for...",
+      "country": "Medium Innovation Countries",
+      "summary": "Just hitting their stride, these countries can have a robust economy and creative sectors, but not potentially enough to set them apart from the best. <br/><br/>"+
+        "Countries include:<br/>"+
+        "<br/>"+
+        "<br/>"+
+        "<br/>",
     },
     {
-      "country": "Country Name3",
-      "summary": "This is a summary for...",
+      "country": "Low Innovation Countries",
+      "summary": "Still working through culture and economic factors, these countries could be the next big thing with a little bit of time. <br/><br/>"+
+        "Countries include:<br/>"+
+        "<br/>"+
+        "<br/>"+
+        "<br/>",
     },
   ];
     bulbDiv.classList.remove("yellow"+value+"-solid")
     bulbDiv.classList.add("yellow"+value)
     for (var i=0; i < array.length; i++) {
-      console.log(value === (i+1))
       if (value !== (i+1)) {
-        console.log(i+1)
         let bulbDiv2 = document.getElementById("bulb"+(i+1));
         bulbDiv2.classList.remove("yellow"+(i+1))
         bulbDiv2.classList.add("yellow"+(i+1)+"-solid")
@@ -31,5 +41,5 @@ function showCountry(value) {
     }
     titleDiv.innerHTML = array[value-1].country;
     summaryDiv.innerHTML = array[value-1].summary;
-    numberDiv.innerHTML = "#"+value;
+    //numberDiv.innerHTML = "#"+value;
 }
